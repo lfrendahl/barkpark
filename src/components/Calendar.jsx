42388a5c59497140c96b5cea4bb08776feee9919
lfrendahl <lfrendahl3@gmail.com>
2023-07-11 17:01:08 -0400

    Added React popupbutton instead of custom button to solve the issue

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
-import { PopupButton } from "react-calendly";
+import { PopupWidget } from 'react-calendly';
 
-const App = () => {
+const PopupComponent = () => {
   return (
-    <div className="App">
-      <PopupButton
-        url="calendly.com/lfrendahl/barkpark-appointment"
-        /*
-         * react-calendly uses React's Portal feature (https://reactjs.org/docs/portals.html) to render the popup modal. As a result, you'll need to
-         * specify the rootElement property to ensure that the modal is inserted into the correct domNode.
-         */
-        rootElement={document.getElementById("root")}
-        text="Click here to schedule!"
+    <div className="popup-widget">
+      <PopupWidget
+        url="https://calendly.com/lfrendahl/barkpark-appointment" 
+        rootElement={document.getElementById('root')}
+        text="Schedule a time"
+        textColor="#ffffff"
+        color="#FF8C42"
       />
     </div>
   );
 };
 
-export default App;
+export default PopupComponent;
